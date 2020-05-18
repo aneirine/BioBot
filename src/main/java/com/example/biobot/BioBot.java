6a903aec442c8a7c6f3c92d65629a1b126357bea
Annchouss <anyashi2002@gmail.com>
2020-05-18 13:33:51 +0300

    Add simple message sending

--- src/main/java/com/example/biobot/BioBot.java
+++ src/main/java/com/example/biobot/BioBot.java
@@ -1,35 +1,54 @@
 package com.example.biobot;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.User;
-
-import java.util.Objects;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public final class BioBot extends TelegramLongPollingBot {
 
     private static final String BOT_NAME = "BioBot";
     private static final String BOT_TOKEN = "887712066:AAH9iCyuutngQo62jxqw0Sf_ObqUNkK7Vlc";
 
+
     @Override
     public void onUpdateReceived(Update update) {
-
+        Message message = update.getMessage();
+        String text = message.getText();
+        if (text.equals("/start")) {
+            sendMessage(message, "Ну привет");
+        }
     }
 
     @Override
     public String getBotUsername() {
-        return null;
+        return BOT_NAME;
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return BOT_TOKEN;
     }
 
+    private void sendMessage(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId());
+        sendMessage.setText(text);
+        try {
+            sendMessageToUser(sendMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    private void sendMessageToUser(SendMessage message) {
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
