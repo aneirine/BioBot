05d4cd1ff5ba6029a221b2c4521e3755eca5dee3
Annchouss <anyashi2002@gmail.com>
2020-05-18 13:11:31 +0300

    Add main methods and fields in BioBot

--- src/main/java/com/example/biobot/BioBot.java
+++ src/main/java/com/example/biobot/BioBot.java
@@ -1,6 +1,40 @@
 package com.example.biobot;
 
-public final class BioBot  {
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 
+public final class BioBot extends TelegramLongPollingBot {
 
+    private static final Logger LOG = LogManager.getLogger(Anonymous.class);
+
+    private final User user;
+    private final Chat chat;
+
+    public BioBot(User user, Chat chat) {
+        if (user == null || chat == null) {
+            LOG.error("USER_CHAT_CANNOT_BE_NULL");
+            throw new IllegalStateException("USER_CHAT_CANNOT_BE_NULL");
+        }
+        this.user = user;
+        this.chat = chat;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
 }
--- src/main/java/com/example/biobot/BioBotApplication.java
+++ src/main/java/com/example/biobot/BioBotApplication.java
@@ -14,7 +14,6 @@ public class BioBotApplication {
 
     public static void main(String[] args) {
 
-
         try {
 
             ApiContextInitializer.init();
