834b4547bf71f5da286ed7da59c3ee763daae97a
Annchouss <anyashi2002@gmail.com>
2020-05-20 18:22:15 +0300

    Add request model

---
+++ src/main/java/com/example/biobot/server/domain/RequestDataModel.java
@@ -0,0 +1,13 @@
+package com.example.biobot.server.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RequestDataModel {
+
+    public String question;
+
+    public String lang;
+}
