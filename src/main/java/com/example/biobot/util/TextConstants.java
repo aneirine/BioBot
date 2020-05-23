e2ddfa08928343d4f2db11045f474ea0e214fae8
Annchouss <anyashi2002@gmail.com>
2020-05-23 22:07:03 +0300

    Add text

--- src/main/java/com/example/biobot/util/TextConstants.java
+++ src/main/java/com/example/biobot/util/TextConstants.java
@@ -14,20 +14,23 @@ public class TextConstants {
     public static final String exclamationEmoji = ":exclamation:";
     public static final String sparklesEmoji = ":sparkles:";
     public static final String doneEmoji = ":white_check_mark:";
+    public static final String questionEmoji = ":question:";
 
     public static final String ENGLISH_START = EmojiParser.parseToUnicode(heartEmoji + " Welcome to BioBot." +
             "\n" + maskEmoji + " This bot can help you to solve different problem with health and find specific information according to your request." +
             "\n" + exclamationEmoji + " Please, before start choose the language. " +
             "\n" + gbEmoji + " If you want english try /english . " +
             "\n" + helpEmoji + " If you don't understand something, try /help ." +
-            "\n" + languageEmoji + " If you want to change language, try /language");
+            "\n" + languageEmoji + " If you want to change language, try /language" +
+            "\n" + questionEmoji + " Ask bot about your problem");
 
     public static final String UKRAINIAN_START = EmojiParser.parseToUnicode(heartEmoji + " Вітаємо у BioBot." +
             "\n" + maskEmoji + " Цей бот створений щоб допомогти Вам з вирішенням різних проблем, пов'язаних зі станом здоров'я, або знайти конкретну інформацію медичного напрямку за вашим запитом. " +
             "\n" + exclamationEmoji + " Будь ласка, перед початком взаємодії оберіть мову." +
             "\n" + uaEmoji + " Якщо бажана мова для вас - українська, використайте /ukrainian . " +
             "\n" + helpEmoji + " Якщо ви чогось не розумієте, спробуйте /help ." +
-            "\n" + languageEmoji + " Якщо ви бажаєте змінити мову, використайте /language");
+            "\n" + languageEmoji + " Якщо ви бажаєте змінити мову, використайте /language" +
+            "\n" + questionEmoji + " Запитайте у бота про свою проблему");
 
     public static final String LANGUAGE_COMMAND = EmojiParser.parseToUnicode(gbEmoji + " If you want English, try /english.\n" + uaEmoji + " Якщо бажана мова для вас - українська, використайте /ukrainian");
     public static final String LANGUAGE_SELECTION = EmojiParser.parseToUnicode(exclamationEmoji + " Please, choose the language /english.\n" + exclamationEmoji + " Будь ласка, оберіть мову /ukrainian.");
